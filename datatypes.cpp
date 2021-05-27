457cb020b3fb8e5e6fb15e6ec8c0aee435c64145
Prathamesh Borse <PrathameshBorse.official@gmail.com>
2021-05-27 11:48:08 +0530

    Day 2 - Datatypes learned

---
+++ datatypes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+    int a; //declaration
+    a=10; // Initialization
+    cout<<"Size of Int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"Size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"Size of Char "<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"Size of bool "<<sizeof(d)<<endl;
+    return 0;
+}
