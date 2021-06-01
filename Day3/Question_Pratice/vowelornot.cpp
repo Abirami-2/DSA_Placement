025423e08f1ab22ea6b14be8fd3f4af9e52fd875
Prathamesh Borse <PrathameshBorse.official@gmail.com>
2021-06-01 15:39:43 +0530

    Day 3 - Question Pratice

---
+++ Day3/Question_Pratice/vowelornot.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main()
+{
+    char c;
+    cout<<"Enter an character: ";
+    cin>>c;
+
+    if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+        cout<<"Character is vowel"<<endl;
+    }else if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+        cout<<"Character is vowel"<<endl;
+    }else{
+        cout<<"Character is not vowel"<<endl;
+    }
+    return 0;
+}
